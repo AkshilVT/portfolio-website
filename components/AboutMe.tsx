aeda4365267648c470b13581bcb90999027798a6
Akshil Thumar <akshilvthumar@gmail.com>
2022-07-31 22:48:09 +0530

    made pages responsive

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,31 +1,33 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 function AboutMe() {
   return (
-    <div className="w-full h-full p-5 mt-10 flex justify-around items-center">
-      <div className="w-full">
-        <div className="w-[200px] mb-5 h-2 light-gradient rounded-full"></div>
-        <h1 className="text-4xl ">Hello, I am</h1>
-        <h6 className="text-7xl my-1">Akshil Thumar</h6>
-        <div className="relative h-10">
-          <div className="h-10 overflow-hidden absolute">
-            <h5 className="text-4xl">Fullstack Developer</h5>
-            <h5 className="text-4xl">Blockchain Enthusiast</h5>
-            <h5 className="text-4xl ">Aspiring Creative Developer</h5>
-            <h5 className="text-4xl ">AR-VR Developer</h5>
+    <div className='w-full h-full p-5 mt-10 flex flex-col lg:flex-row overflow-hidden justify-around items-center'>
+      <div className='w-full lg:pl-5 xl:pl-20'>
+        <div className='w-full lg:w-[200px] mb-5 h-2 light-gradient rounded-full'></div>
+        <h1 className='lg:text-4xl text-xl'>Hello, I am</h1>
+        <h6 className='text-4xl lg:text-7xl my-1'>Akshil Thumar</h6>
+        <div className='relative h-10'>
+          <div className='h-10 overflow-hidden absolute'>
+            <h5 className='text-xl lg:text-4xl'>Fullstack Developer</h5>
+            <h5 className='text-xl lg:text-4xl'>Blockchain Enthusiast</h5>
+            <h5 className='text-xl lg:text-4xl '>
+              Aspiring Creative Developer
+            </h5>
+            <h5 className='text-xl lg:text-4xl '>AR-VR Developer</h5>
           </div>
         </div>
-        <div className="w-[200px] mt-5 h-2 light-gradient rounded-full"></div>
+        <div className='w-full lg:w-[200px] mt-5 h-2 light-gradient rounded-full'></div>
       </div>
-      <aside className="w-full h-full">
+      <aside className='w-full h-full mt-5 lg:mt-0'>
         <Image
-          className="rounded-full"
-          src={"/photo.png"}
-          width="500px"
-          height="500px"
-          layout="responsive"
-          alt="portrait"
+          className=''
+          src={'/photo2.png'}
+          width='500px'
+          height='500px'
+          layout='responsive'
+          alt='portrait'
         ></Image>
       </aside>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import type { NextPage } from "next";
-import React from "react";
-import Contact from "../components/Contact";
-import Experience from "../components/Experience";
-import Project from "../components/Project";
-const AboutMe = React.lazy(() => import("../components/AboutMe"));
-const CustomCursor = React.lazy(() => import("../components/CustomCursor.js"));
-const Navbar = React.lazy(() => import("../components/Navbar"));
+import type { NextPage } from 'next';
+import React from 'react';
+import Contact from '../components/Contact';
+import Experience from '../components/Experience';
+import Project from '../components/Project';
+const AboutMe = React.lazy(() => import('../components/AboutMe'));
+const CustomCursor = React.lazy(() => import('../components/CustomCursor.js'));
+const Navbar = React.lazy(() => import('../components/Navbar'));
 
 const Home: NextPage = () => {
   const [active, setActive] = React.useState(0);
   return (
-    <div className="w-full bg-gray-100 h-screen">
-      <div className="max-w-screen-2xl mx-auto">
-        <CustomCursor />
-        <section className="w-full bg-gray-100 text-white p-5">
+    <div className='w-full bg-gray-100 h-screen overflow-hidden'>
+      <div className='max-w-screen-2xl mx-auto'>
+        <div className='hidden lg:block'>
+          <CustomCursor />
+        </div>
+        <section className='w-full bg-gray-100 text-white p-5'>
           <Navbar active={active} setActive={setActive} />
         </section>
         {active == 0 && <AboutMe />}
