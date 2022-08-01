23a913e84cd762584e90b0d35ff95c3e465bef44
Akshil Thumar <akshilvthumar@gmail.com>
2022-08-01 14:22:14 +0530

    added responsive navbar

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 
 function AboutMe() {
   return (
-    <div className='w-full h-full p-5 mt-10 flex flex-col lg:flex-row overflow-hidden justify-around items-center'>
-      <div className='w-full lg:pl-5 xl:pl-20'>
-        <div className='w-full lg:w-[200px] mb-5 h-2 light-gradient rounded-full'></div>
-        <h1 className='lg:text-4xl text-xl'>Hello, I am</h1>
-        <h6 className='text-4xl lg:text-7xl my-1'>Akshil Thumar</h6>
-        <div className='relative h-10'>
+    <div className='w-full h-full p-5 mt-10 flex flex-col xl:flex-row overflow-hidden lg:justify-around items-center'>
+      <div className='w-full lg:pl-5 xl:pl-20 text-center xl:text-left'>
+        <div className='w-full xl:w-[200px] mb-5 h-2 light-gradient rounded-full'></div>
+        <h1 className='xl:text-4xl text-xl'>Hello, I am</h1>
+        <h6 className='text-4xl xl:text-7xl my-1'>Akshil Thumar</h6>
+        <div className='relative h-10 flex justify-center xl:justify-start'>
           <div className='h-10 overflow-hidden absolute'>
-            <h5 className='text-xl lg:text-4xl'>Fullstack Developer</h5>
-            <h5 className='text-xl lg:text-4xl'>Blockchain Enthusiast</h5>
-            <h5 className='text-xl lg:text-4xl '>
+            <h5 className='text-xl xl:text-4xl'>Fullstack Developer</h5>
+            <h5 className='text-xl xl:text-4xl'>Blockchain Enthusiast</h5>
+            <h5 className='text-xl xl:text-4xl '>
               Aspiring Creative Developer
             </h5>
-            <h5 className='text-xl lg:text-4xl '>AR-VR Developer</h5>
+            <h5 className='text-xl xl:text-4xl '>AR-VR Developer</h5>
           </div>
         </div>
-        <div className='w-full lg:w-[200px] mt-5 h-2 light-gradient rounded-full'></div>
+        <div className='w-full xl:w-[200px] mt-5 h-2 light-gradient rounded-full'></div>
       </div>
-      <aside className='w-full h-full mt-5 lg:mt-0'>
+      <aside className='w-full h-full mt-5 xl:mt-0'>
         <Image
           className=''
           src={'/photo2.png'}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,69 +1,167 @@
-import { Card } from "@nextui-org/react";
-import router from "next/router";
-import React from "react";
-
+import { Button, Card } from '@nextui-org/react';
+import router from 'next/router';
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 function Navbar({ setActive, active }: any) {
+  const [showNav, setShowNav] = React.useState(false);
   return (
-    <nav className="w-full flex justify-center space-x-5">
-      <Card
-        isPressable
-        isHoverable={active != 0 ? true : false}
-        variant={active == 0 ? "shadow" : "flat"}
-        className="nav-link "
-        onClick={() => setActive(0)}
-      >
-        <a href="#" className="cursor-none">
-          About Me
-        </a>
-      </Card>
-      <Card
-        isHoverable={active != 1 ? true : false}
-        isPressable
-        variant={active == 1 ? "shadow" : "flat"}
-        className="nav-link max-w-[250px]"
-        onClick={() => setActive(1)}
-      >
-        <a href="#" className="cursor-none">
-          Projects & Tech Stack
-        </a>
-      </Card>
-      <Card
-        isHoverable={active != 2 ? true : false}
-        isPressable
-        variant={active == 2 ? "shadow" : "flat"}
-        className="nav-link max-w-[270px]"
-        onClick={() => setActive(2)}
-      >
-        <a href="#" className="cursor-none">
-          Education & Experience
-        </a>
-      </Card>
-      <Card
-        isHoverable={active != 3 ? true : false}
-        isPressable
-        variant={active == 3 ? "shadow" : "flat"}
-        className="nav-link "
-        onClick={() => setActive(3)}
-      >
-        <a href="#" className="cursor-none">
-          Contact Me
-        </a>
-      </Card>
-      <Card
-        isHoverable={active != 4 ? true : false}
-        isPressable
-        variant={active == 4 ? "shadow" : "bordered"}
-        className="nav-link bg-gray-100"
+    <>
+      <Button
+        className='lg:hidden nav-link z-10 min-w-min'
         onClick={() => {
-          setActive(4);
-          router.push("/scene");
+          setShowNav(!showNav);
         }}
       >
-        <a href="#" className="cursor-none">
-          Playground
-        </a>
-      </Card>
-    </nav>
+        <GiHamburgerMenu />
+      </Button>
+      {showNav && (
+        <>
+          <div className='lg:hidden p-5 fixed inset-10 rounded-lg backdrop-blur bg-white/40 z-50 border-2 border-white/20 shadow-xl duration-100 transition-all'>
+            <GrClose
+              className='float-right'
+              onClick={() => {
+                setShowNav(false);
+              }}
+            />
+            <div className='flex flex-col space-y-5 justify-center text-black items-center h-full'>
+              <h1 className='text-4xl'>Navigate to</h1>
+              <p
+                onClick={() => {
+                  if (active != 0) {
+                    setActive(0);
+                    setShowNav(false);
+                  }
+                }}
+                className={
+                  'p-2 text-2xl ' + (active == 0 ? 'border-b border-black' : '')
+                }
+              >
+                About me
+              </p>
+              <p
+                onClick={() => {
+                  if (active != 1) {
+                    setShowNav(false);
+                    setActive(1);
+                  }
+                }}
+                className={
+                  'p-2 text-2xl ' + (active == 1 ? 'border-b border-black' : '')
+                }
+              >
+                Project & Tech Stack
+              </p>
+              <p
+                onClick={() => {
+                  if (active != 2) {
+                    setShowNav(false);
+                    setActive(2);
+                  }
+                }}
+                className={
+                  'p-2 text-2xl ' + (active == 2 ? 'border-b border-black' : '')
+                }
+              >
+                Contact me
+              </p>
+              <p
+                onClick={() => {
+                  if (active != 3) {
+                    setShowNav(false);
+                    setActive(3);
+                  }
+                }}
+                className={
+                  'p-2 text-2xl ' + (active == 3 ? 'border-b border-black' : '')
+                }
+              >
+                Education & Experience
+              </p>
+              <p
+                className={
+                  'p-2 text-xl text-black/50 ' +
+                  (active == 4 ? 'border-b border-black' : '')
+                }
+              >
+                <span className=' line-through'>Playground</span> (available in
+                Desktop)
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+      <nav className='w-full hidden lg:flex justify-center space-x-5'>
+        <div className='w-full'>
+          <Card
+            isPressable
+            isHoverable={active != 0 ? true : false}
+            variant={active == 0 ? 'shadow' : 'flat'}
+            className='nav-link '
+            onClick={() => setActive(0)}
+          >
+            <a href='#' className='cursor-none'>
+              About Me
+            </a>
+          </Card>
+        </div>
+        <div className='w-full'>
+          <Card
+            isHoverable={active != 1 ? true : false}
+            isPressable
+            variant={active == 1 ? 'shadow' : 'flat'}
+            className='nav-link '
+            onClick={() => setActive(1)}
+          >
+            <a href='#' className='cursor-none'>
+              Projects & Tech Stack
+            </a>
+          </Card>
+        </div>
+        <div className='w-full'>
+          <Card
+            isHoverable={active != 3 ? true : false}
+            isPressable
+            variant={active == 3 ? 'shadow' : 'flat'}
+            className='nav-link '
+            onClick={() => setActive(3)}
+          >
+            <a href='#' className='cursor-none'>
+              Contact Me
+            </a>
+          </Card>
+        </div>
+        <div className='w-full'>
+          <Card
+            isHoverable={active != 2 ? true : false}
+            isPressable
+            variant={active == 2 ? 'shadow' : 'flat'}
+            className='nav-link '
+            onClick={() => setActive(2)}
+          >
+            <a href='#' className='cursor-none'>
+              Education & Experience
+            </a>
+          </Card>
+        </div>
+        <div className='w-full'>
+          <Card
+            isHoverable={active != 4 ? true : false}
+            isPressable
+            variant={active == 4 ? 'shadow' : 'bordered'}
+            className='nav-link bg-gray-100'
+            onClick={() => {
+              setActive(4);
+              router.push('/scene');
+            }}
+          >
+            <a href='#' className='cursor-none'>
+              Playground
+            </a>
+          </Card>
+        </div>
+      </nav>
+    </>
   );
 }
 
